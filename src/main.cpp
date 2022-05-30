8c4e83481c69cc055013163a96c415ad61d67753
mohammadrezababaeimosleh <mohammadrezababaeimosleh@gmail.com>
2022-05-30 11:34:17 -1000

    ending q2 with sorting

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
 #include <gtest/gtest.h>
 
-#include "q1.h"
-#include "q2.h"
-#include "q3.h"
-#include "q4.h"
+
+// #include "q1.h"
+// #include "q2.h"
+// #include "q3.h"
+// #include "q4.h"
+
 
 double power(double x) { return x*x ;}
 
@@ -12,10 +14,11 @@ int main(int argc, char **argv)
 {
     if (false) // make false to run unit-tests
     {
-        double x{2.0};
-        std::cout << power(x) << std::endl;
-        std::cout << q1::derivative(3 , power) << std::endl;
-        std::cout << q1::gradient_descent(-5, 0.1, power) << std::endl;
+        // std::vector<q2::Patient> mamad{q2::read_file("/usr/src/app/resources/lung_cancer.csv")};
+        // std::cout << mamad[0].name << std::endl;
+        // q2::sort(mamad);
+        // std::cout << mamad[0].name << std::endl;
+
 
     }
     else
